fdf0ecdcbad13eaef9b8c147de1c0a9d0c4bbb5c
odai atef <oatef@css.org.sa>
2020-10-05 09:11:13 +0300

    class logs

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+include_once("./vendor/autoload.php");
+
+class logs_hub{
+    public $env;
+    function __construct(){
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+        $this->env=$_ENV;
+    }
+    function log($msg, $application, $level,$execution_time, $environment, $user_id=null, $extra_data=null){
+        $now =time();
+        $data=[
+            "message"=> $msg,
+            "level"=> $level,
+            "application"=> $application,
+            "environment"=> $environment,
+            "user_id"=> $user_id,
+            "execution_time"=>$execution_time,
+            "extra_data"=> $extra_data,
+            "timestamp"=> $now
+        ];
+
+        error_log(json_encode($data), 3, $this->env['DIR']."$now.log");
+        return $data;
+    }
+    function warning($msg, $application, $execution_time, $environment, $user_id=null, $extra_data=null){
+        $this->log($msg, $application, $this->env['WARNING'],$execution_time, $environment, $user_id, $extra_data);
+    }
+    function info($msg, $application, $execution_time, $environment, $user_id=null, $extra_data=null){
+        $this->log($msg, $application, $this->env['INFO'],$execution_time, $environment, $user_id, $extra_data);
+    }
+    function error($msg, $application, $execution_time, $environment, $user_id=null, $extra_data=null){
+        $this->log($msg, $application, $this->env['ERROR'],$execution_time, $environment, $user_id, $extra_data);
+    }
+    function critical($msg, $application, $execution_time, $environment, $user_id=null, $extra_data=null){
+        $this->notify($this->log($msg, $application, $this->env['CRITICAL'],$execution_time, $environment, $user_id, $extra_data));
+    }
+    function notify($data){
+        $payload = json_encode($data);
+        $ch = curl_init('URL');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($payload))
+        );
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+}
+?>
